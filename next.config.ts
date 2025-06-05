018d40fb8ab673b12b87d92275445eae47e6df69
tonyleuk <tonyleuk@gmail.com>
2025-06-05 19:17:21 +0100

    couldfix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 /**
  * @type {import('next').NextConfig}
  */
-/* const isProd = process.env.NODE_ENV === 'production'; */
+//const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
-  /* basePath: isProd ? '/DigitalLetter' : '', 
-  assetPrefix: isProd ? '/DigitalLetter' : '',  */
+  basePath: '/DigitalLetter', /*  isProd ? '/DigitalLetter' : '', */
   output: 'export',
   distDir: 'dist',
   images: {
