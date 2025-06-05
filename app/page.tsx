4bbaff234f17d704a4bcaab8d0c9b685639eceaf
tonyleuk <tonyleuk@gmail.com>
2025-06-05 18:28:38 +0100

    helloworld

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Image from "next/image";
+import { motion } from 'motion/react';
+
+import Letter from './letter';
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex justify-center">  
+        <motion.div
+        initial={{ y: 50 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 2, ease: 'easeOut' }}
+        >
+          <Letter
+            src="/images/photo.jpg"
+            alt="Example"
+            title="This is a title"
+            description="This is a description under the image."
+          />
+        </motion.div>
+      </main>
+    </>
+  );
+}
